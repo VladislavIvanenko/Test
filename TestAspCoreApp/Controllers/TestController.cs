58326454b7fddc85cb1053953c13e5bb19f21505
Владислав Иваненко <vladislav.ivanenko94@gmail.com>
2019-03-28 22:48:23 +0200

    Init commit!!!

---
+++ TestAspCoreApp/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAspCoreApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("https://metanit.com/sharp/aspnet5/20.1.php");
+        }
+    }
+}
